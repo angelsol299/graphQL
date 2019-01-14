3e8aed9357a70213449b0bad00b654dd97752dd4
angelsol299 <angelsol299@hotmail.com>
2019-01-14 20:49:32 +0100

    schema dummy data authors

--- server/models/book.js
+++ server/models/book.js
@@ -1,4 +1,8 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const bookSchema = new Schema({});
+const bookSchema = new Schema({
+  name: String,
+  genre: String,
+  authorId: String
+});
