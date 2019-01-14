02014b86ac095ae6f96f4ffa1a7f941b976edf11
angelsol299 <angelsol299@hotmail.com>
2019-01-14 20:43:50 +0100

    schema dummy data authors

--- server/app.js
+++ server/app.js
@@ -6,6 +6,12 @@ const schema = require("./schema/schema");
 const mongoose = require("mongoose");
 const app = express();
 
+//connect to mlab database
+// make sure to replace my db string & creds with your own
+mongoose.connect(
+  "mongodb://<dbuser>:<dbpassword>@ds133077.mlab.com:33077/gql-ninja"
+);
+
 app.use(
   "/graphql",
   graphqlHTTP({
